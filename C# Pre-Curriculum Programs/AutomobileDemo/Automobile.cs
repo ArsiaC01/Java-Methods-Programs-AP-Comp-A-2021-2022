5d5e26f02cc18574df5eab10e5269c5c1b2d2cc9
ArsiaC01 <95257937+ArsiaC01@users.noreply.github.com>
2022-09-01 12:52:13 -0400

    Added AutomobileDemo
    File reorganization + Automobile Demo C# Program

---
+++ C# Pre-Curriculum Programs/AutomobileDemo/Automobile.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class Automobile
+{
+	private int idNum;
+	private int make;
+	private int year;
+	private double price;
+
+	public Automobile(int idNum, int make, int year, double price)
+	{
+		this.idNum = idNum;
+		this.make = make;
+		this.year = year;
+		this.price = price;
+	}
+
+	public override string ToString()
+    {
+		string theString;
+		theString = $"idNum: {idNum}\n" +
+            $"Make: {make}\n" +
+            $"Year: {year}\n" +
+            $"Price: {price}";
+		return theString;
+    }
+
+	public int IdNum
+    {
+        get { return idNum; }
+    }
+	public int Make
+    {
+		get { return make; }
+    }
+	public int Year
+    {
+		get { return year; }
+    }
+	public double Price
+    {
+		get { return price; }
+    }
+}
---
+++ C# Pre-Curriculum Programs/AutomobileDemo/AutomobileDemo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomobileDemo
+{
+    internal class AutomobileDemo
+    {
+        public static void Main()
+        {
+            const int length = 8;
+            Automobile[] autoArray = new Automobile[8]; 
+            for(int i = 0; i < length; i++)
+            {
+                
+                Console.WriteLine($"Input data for automobile {i}");
+                Console.Write("Make: ");
+                int make = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Year: ");
+                int year = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Price: ");
+                double price = Convert.ToDouble(Console.ReadLine());
+
+                autoArray[i] = new Automobile(i, make, year, price);
+            }
+
+            foreach (Automobile auto in autoArray)
+            {
+                Console.WriteLine(auto.ToString());
+                Console.WriteLine();
+            }
+        }
+    }
+}
