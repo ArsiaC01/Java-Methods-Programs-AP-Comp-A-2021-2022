39285e5f465bb22142ce8a490114defad2ebf4a1
Gavin Tran <gavinpha@outlook.com>
2023-01-22 21:34:30 -0500

    Conformed to Java conventions and updated class.

--- Chapter 09 Programs/Pg261-09-14aCoins/Main.java
+++ Chapter 09 Programs/Pg261-09-14aCoins/Main.java
@@ -3,22 +3,20 @@ package com.company;
 
 import java.util.Scanner;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
-        Scanner scanner1 = new Scanner(System.in);
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Please input the amount of coins: ");
-        int coins = scanner1.nextInt();
+        int coins = scanner.nextInt();
         Coins coin1 = new Coins(coins);
 
         System.out.println(coins + " coins can be represented as " +
                 coin1.getQuarters() + " quarters, " +
                 coin1.getDimes() + " dimes, " +
-                coin1.getNickles() + " nickles, " +
+                coin1.getNickles() + " nickles, and " +
                 coin1.getPennies() + " pennies."
         );
 
-        scanner1.close();
+        scanner.close();
     }
 }
