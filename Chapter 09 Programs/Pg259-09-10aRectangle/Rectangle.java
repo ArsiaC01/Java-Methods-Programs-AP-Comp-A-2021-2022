7c26a76715171d79e8b74968eb61117614ec9cc7
Gavin Tran <gavinpha@outlook.com>
2023-01-23 20:47:29 -0500

    Conformed to Java conventions and updated Rectangle directory.

--- Chapter 09 Programs/Pg259-09-10aRectangle/Rectangle.java
+++ Chapter 09 Programs/Pg259-09-10aRectangle/Rectangle.java
@@ -1,43 +1,62 @@
 // Code written as part of the Java-Methods-Program-AP-Comp-A-2021-2022 repository on GitHub.
 import java.lang.Math;
 
-public class Rectangle
-{
+/**
+ * A class representing a simple 2D rectangle.
+ */
+public class Rectangle {
     private int width;
     private int height;
 
-    public Rectangle(int width, int height)
-    {
-        if (width > 0)
+    /**
+     * Constructs a {@link Rectangle} by width and height.
+     * @param width The width. Defaults to 1 if a non-positive width is given.
+     * @param height The height. Defaults to 1 in a non-positive height is given.
+     */
+    public Rectangle(int width, int height) {
+        if (width <= 0) {
+            this.width = 1;
+        } else {
             this.width = width;
-        if (height > 0)
+        }
+        if (height <= 0) {
+            this.height = 1;
+        } else {
             this.height = height;
+        }
+    }
 
+    /**
+     * Constructs a square {@link Rectangle} object with the given side length.
+     * @param sideLength The side length.
+     */
+    public Rectangle(int sideLength) {
+        width = sideLength;
+        height = sideLength;
     }
-    public Rectangle(int singleSize)
-    {
-        width = singleSize;
-        height = singleSize;
-    }
-    public Rectangle ()
-    {
+
+    /**
+     * Constructs a default square {@link Rectangle} object with a width and height of 1.
+     */
+    public Rectangle() {
         width = 1;
         height = 1;
     }
 
-    public String isSquare()
-    {
-        if (width == height)
-            return "is";
-        else
-            return "isn't";
+    /**
+     * @return If this rectangle has an equal height and width, i.e., if it is a square.
+     */
+    public boolean isSquare() {
+        return width == height;
     }
 
-    public int Quadratize()
-    {
+    /**
+     * Calculates the side length of the square closest in area to this rectangle.
+     * @return An integer side length.
+     */
+    public int quadratize() {
         double area = width * height;
         double newSideLength = Math.sqrt(area);
         return (int) newSideLength;
-
     }
 }
