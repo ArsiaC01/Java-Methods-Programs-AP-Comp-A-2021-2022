ab4281b99cdd90a18edc4e76e3265af3fa583193
Gavin Tran <gavinpha@outlook.com>
2023-01-27 04:35:49 -0500

    Conformed to Java conventions and updated all directories of Chapter 5.

--- src/main/java/chapter05/movingdiskandmorning/EasySound.java
+++ src/main/java/chapter05/movingdiskandmorning/EasySound.java
@@ -1,64 +1,50 @@
-package chapter05.movingdiskandmorning;// Code written as part of the Java-Methods-Program-AP-Comp-A-2021-2022 repository on GitHub.
+// Code written as part of the Java-Methods-Program-AP-Comp-A-2021-2022 repository on GitHub.
+package chapter05.movingdiskandmorning;
+
+import javax.sound.sampled.*;
+import javax.sound.sampled.DataLine.Info;
 import java.io.File;
 import java.io.IOException;
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.DataLine;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.SourceDataLine;
 
-public class EasySound
-{
-  private SourceDataLine line = null;
-  private byte[] audioBytes;
-  private int numBytes;
+public class EasySound {
+    private SourceDataLine line = null;
+    private byte[] audioBytes;
+    private int numBytes;
 
-  public EasySound(String fileName)
-  {
-    File  soundFile = new File(fileName);
-    AudioInputStream audioInputStream = null;
-    try
-    {
-      audioInputStream = AudioSystem.getAudioInputStream(soundFile);
-    }
-    catch (Exception ex)
-    {
-      System.out.println("*** Cannot find " + fileName + " ***");
-      System.exit(1);
-    }
+    public EasySound(String fileName) {
+        File soundFile = new File(fileName);
+        AudioInputStream audioInputStream = null;
+        try {
+            audioInputStream = AudioSystem.getAudioInputStream(soundFile);
+        } catch (Exception ex) {
+            System.err.println("*** Cannot find " + fileName + " ***");
+            System.exit(1);
+        }
 
-    AudioFormat audioFormat = audioInputStream.getFormat();
-    DataLine.Info info = new DataLine.Info(SourceDataLine.class,
-                         audioFormat);
-    try
-    {
-      line = (SourceDataLine)AudioSystem.getLine(info);
-      line.open(audioFormat);
-    }
-    catch (LineUnavailableException ex)
-    {
-      System.out.println("*** Audio line unavailable ***");
-      System.exit(1);
+        AudioFormat audioFormat = audioInputStream.getFormat();
+        Info info = new Info(SourceDataLine.class,
+                audioFormat);
+        try {
+            line = (SourceDataLine) AudioSystem.getLine(info);
+            line.open(audioFormat);
+        } catch (LineUnavailableException ex) {
+            System.err.println("*** Audio line unavailable ***");
+            System.exit(1);
+        }
+
+        line.start();
+
+        audioBytes = new byte[(int) soundFile.length()];
+
+        try {
+            numBytes = audioInputStream.read(audioBytes, 0, audioBytes.length);
+        } catch (IOException ex) {
+            System.out.println("*** Cannot read " + fileName + " ***");
+            System.exit(1);
+        }
     }
 
-    line.start();
-
-    audioBytes = new byte[(int)soundFile.length()];
-
-    try
-    {
-      numBytes = audioInputStream.read(audioBytes, 0, audioBytes.length);
+    public void play() {
+        line.write(audioBytes, 0, numBytes);
     }
-    catch (IOException ex)
-    {
-      System.out.println("*** Cannot read " + fileName + " ***");
-      System.exit(1);
-    }
-  }
-
-  public void play()
-  {
-    line.write(audioBytes, 0, numBytes);
-  }
 }
--- src/main/java/chapter05/movingdiskandmorning/Morning.java
+++ src/main/java/chapter05/movingdiskandmorning/Morning.java
@@ -1,41 +1,39 @@
-package chapter05.movingdiskandmorning;// Code written as part of the Java-Methods-Program-AP-Comp-A-2021-2022 repository on GitHub.
+// Code written as part of the Java-Methods-Program-AP-Comp-A-2021-2022 repository on GitHub.
+package chapter05.movingdiskandmorning;
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class Morning extends JFrame implements ActionListener
-{
-  private EasySound rooster;
-  private int time;
-  /**
-   *   Constructor
-   */
-  public Morning()
-  {
-    super("Morning");
-    rooster = new EasySound("roost.wav");
+public class Morning extends JFrame implements ActionListener {
+    private EasySound rooster;
+    private int time;
 
-    Container c = getContentPane();
-    c.setBackground(Color.WHITE);
-    
-    time = 0;
-    Timer clock = new Timer(30, this); 
-    clock.start();
-  }
-  
-  public void actionPerformed(ActionEvent e)
-  {
-    time++;
-    if ((time % 50) == 0)
-    	rooster.play();
-    repaint();
-  }
+    public Morning() {
+        super("Morning");
+        rooster = new EasySound("roost.wav");
 
-  public static void main(String args[])
-  {
-    Morning morning = new Morning();
-    morning.setSize(300, 150);
-    morning.setDefaultCloseOperation(EXIT_ON_CLOSE);
-    morning.setVisible(true);
-  }
+        Container c = getContentPane();
+        c.setBackground(Color.WHITE);
+
+        time = 0;
+        Timer clock = new Timer(30, this);
+        clock.start();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        time++;
+        if (time % 50 == 0) {
+            rooster.play();
+        }
+        repaint();
+    }
+
+    public static void main(String[] args) {
+        Morning morning = new Morning();
+        morning.setSize(300, 150);
+        morning.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        morning.setVisible(true);
+    }
 }
